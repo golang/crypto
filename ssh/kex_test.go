665213f561b1d86f94956515da4886141e45eb9b
Alex Brainman <alex.brainman@gmail.com>
2013-08-29 12:57:42 +1000

    go.crypto/ssh: use 127.0.0.1 during TestKexAlgorithms (fixes windows build)
    R=golang-dev, mikioh.mikioh, remyoudompheng
    CC=golang-dev
    https://golang.org/cl/13370043

--- ssh/kex_test.go
+++ ssh/kex_test.go
@@ -13,7 +13,7 @@ import (
 )
 
 func pipe() (net.Conn, net.Conn, error) {
-	l, err := net.Listen("tcp", ":0")
+	l, err := net.Listen("tcp", "127.0.0.1:0")
 	if err != nil {
 		return nil, nil, err
 	}
