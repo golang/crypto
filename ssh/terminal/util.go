63a71ca82d5ccbe458cc4acef0c0ea8076463976
Brad Fitzpatrick <bradfitz@golang.org>
2014-01-13 15:00:50 -0800

    undo CL 51690043 / abf8f8812575
    Breaks FreeBSD build of subrepo for non-tip users.
    
    ««« original CL description
    go.crypto/ssh/terminal: enable freebsd build
    
    syscall.Termios, which was the only thing breaking the build, is
    available in go tip now
    (https://code.google.com/p/go/source/detail?r=873d664b00ec)
    
    R=golang-codereviews, bradfitz
    CC=golang-codereviews
    https://golang.org/cl/51690043
    
    »»»
    
    R=golang-codereviews, dave
    CC=golang-codereviews
    https://golang.org/cl/51100044

--- ssh/terminal/util.go
+++ ssh/terminal/util.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build linux,!appengine darwin freebsd
+// +build linux,!appengine darwin
 
 // Package terminal provides support functions for dealing with terminals, as
 // commonly found on UNIX systems.
