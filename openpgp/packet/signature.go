a1006296b90ae8bbdb11c2b0f22ba76a8d0b30e5
David Leon Gil <dgil@yahoo-inc.com>
2014-12-10 11:52:50 -0800

    Make E2E keys readable.

--- openpgp/packet/signature.go
+++ openpgp/packet/signature.go
@@ -9,12 +9,13 @@ import (
 	"crypto/dsa"
 	"crypto/rsa"
 	"encoding/binary"
-	"golang.org/x/crypto/openpgp/errors"
-	"golang.org/x/crypto/openpgp/s2k"
 	"hash"
 	"io"
 	"strconv"
 	"time"
+
+	"golang.org/x/crypto/openpgp/errors"
+	"golang.org/x/crypto/openpgp/s2k"
 )
 
 const (
@@ -62,6 +63,10 @@ type Signature struct {
 	// See RFC 4880, section 5.2.3.23 for details.
 	RevocationReason     *uint8
 	RevocationReasonText string
+
+	// MDC is set if this signature has a feature packet that indicates
+	// support for MDC subpackets.
+	MDC bool
 
 	outSubpackets []outputSubpacket
 }
@@ -190,6 +195,7 @@ const (
 	primaryUserIdSubpacket       signatureSubpacketType = 25
 	keyFlagsSubpacket            signatureSubpacketType = 27
 	reasonForRevocationSubpacket signatureSubpacketType = 29
+	featuresSubpacket            signatureSubpacketType = 30
 )
 
 // parseSignatureSubpacket parses a single subpacket. len(subpacket) is >= 1.
@@ -343,7 +349,13 @@ func parseSignatureSubpacket(sig *Signature, subpacket []byte, isHashed bool) (r
 		sig.RevocationReason = new(uint8)
 		*sig.RevocationReason = subpacket[0]
 		sig.RevocationReasonText = string(subpacket[1:])
-
+	case featuresSubpacket:
+		if len(subpacket) != 1 {
+			err = errors.StructuralError("more than one feature in features subpacket")
+		}
+		if (subpacket[0] | 0x80) != 0x81 {
+			err = errors.StructuralError("unknown feature -- besides MDC -- in features subpacket")
+		}
 	default:
 		if isCritical {
 			err = errors.UnsupportedError("unknown critical signature subpacket type " + strconv.Itoa(int(packetType)))
