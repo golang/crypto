85ce60fb243044b555fe7b6690e555d0c089831f
Brad Fitzpatrick <bradfitz@golang.org>
2016-10-12 21:50:29 +0000

    poly1305: fix build
    Updates golang/go#17422
    
    Change-Id: Ie5f16e24f87b3d800f1182b5b09d6cf638135e33
    Reviewed-on: https://go-review.googlesource.com/30970
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Run-TryBot: Brad Fitzpatrick <bradfitz@golang.org>
    Reviewed-by: Adam Langley <agl@golang.org>

--- poly1305/sum_arm.s
+++ poly1305/sum_arm.s
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-#include "textflags.h"
+#include "textflag.h"
 
 // This code was translated into a form compatible with 5a from the public
 // domain source by Andrew Moon: github.com/floodyberry/poly1305-opt/blob/master/app/extensions/poly1305.
