1f3ad9b59903114cdbd290dc486e84d3c078c4c9
小林照幸 <koba-ninkigumi@users.noreply.github.com>
2016-11-27 10:11:30 +0900

    fix Compile Error
    composite struct literal encoding/asn1.ObjectIdentifier with unkeyed fields

--- ocsp/ocsp.go
+++ ocsp/ocsp.go
@@ -137,18 +137,18 @@ type revokedInfo struct {
 }
 
 var (
-	oidSignatureMD2WithRSA      = asn1.ObjectIdentifier{1, 2, 840, 113549, 1, 1, 2}
-	oidSignatureMD5WithRSA      = asn1.ObjectIdentifier{1, 2, 840, 113549, 1, 1, 4}
-	oidSignatureSHA1WithRSA     = asn1.ObjectIdentifier{1, 2, 840, 113549, 1, 1, 5}
-	oidSignatureSHA256WithRSA   = asn1.ObjectIdentifier{1, 2, 840, 113549, 1, 1, 11}
-	oidSignatureSHA384WithRSA   = asn1.ObjectIdentifier{1, 2, 840, 113549, 1, 1, 12}
-	oidSignatureSHA512WithRSA   = asn1.ObjectIdentifier{1, 2, 840, 113549, 1, 1, 13}
-	oidSignatureDSAWithSHA1     = asn1.ObjectIdentifier{1, 2, 840, 10040, 4, 3}
-	oidSignatureDSAWithSHA256   = asn1.ObjectIdentifier{2, 16, 840, 1, 101, 3, 4, 3, 2}
-	oidSignatureECDSAWithSHA1   = asn1.ObjectIdentifier{1, 2, 840, 10045, 4, 1}
-	oidSignatureECDSAWithSHA256 = asn1.ObjectIdentifier{1, 2, 840, 10045, 4, 3, 2}
-	oidSignatureECDSAWithSHA384 = asn1.ObjectIdentifier{1, 2, 840, 10045, 4, 3, 3}
-	oidSignatureECDSAWithSHA512 = asn1.ObjectIdentifier{1, 2, 840, 10045, 4, 3, 4}
+	oidSignatureMD2WithRSA      = asn1.ObjectIdentifier([]int{1, 2, 840, 113549, 1, 1, 2})
+	oidSignatureMD5WithRSA      = asn1.ObjectIdentifier([]int{1, 2, 840, 113549, 1, 1, 4})
+	oidSignatureSHA1WithRSA     = asn1.ObjectIdentifier([]int{1, 2, 840, 113549, 1, 1, 5})
+	oidSignatureSHA256WithRSA   = asn1.ObjectIdentifier([]int{1, 2, 840, 113549, 1, 1, 11})
+	oidSignatureSHA384WithRSA   = asn1.ObjectIdentifier([]int{1, 2, 840, 113549, 1, 1, 12})
+	oidSignatureSHA512WithRSA   = asn1.ObjectIdentifier([]int{1, 2, 840, 113549, 1, 1, 13})
+	oidSignatureDSAWithSHA1     = asn1.ObjectIdentifier([]int{1, 2, 840, 10040, 4, 3})
+	oidSignatureDSAWithSHA256   = asn1.ObjectIdentifier([]int{2, 16, 840, 1, 101, 3, 4, 3, 2})
+	oidSignatureECDSAWithSHA1   = asn1.ObjectIdentifier([]int{1, 2, 840, 10045, 4, 1})
+	oidSignatureECDSAWithSHA256 = asn1.ObjectIdentifier([]int{1, 2, 840, 10045, 4, 3, 2})
+	oidSignatureECDSAWithSHA384 = asn1.ObjectIdentifier([]int{1, 2, 840, 10045, 4, 3, 3})
+	oidSignatureECDSAWithSHA512 = asn1.ObjectIdentifier([]int{1, 2, 840, 10045, 4, 3, 4})
 )
 
 var hashOIDs = map[crypto.Hash]asn1.ObjectIdentifier{
