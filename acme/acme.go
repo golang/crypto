541b9d50ad47e36efd8fb423e938e59ff1691f68
Kevin Burke <kev@inburke.com>
2017-10-19 08:52:39 -0700

    acme, acme/autocert: add missing articles
    Some sentences are missing the word "the" and others have it too
    often.
    
    Change-Id: I01a631e8f2655dea4433a73e75cbc94e0c8959c2
    Reviewed-on: https://go-review.googlesource.com/71870
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

--- acme/acme.go
+++ acme/acme.go
@@ -142,7 +142,7 @@ func (c *Client) Discover(ctx context.Context) (Directory, error) {
 //
 // In the case where CA server does not provide the issued certificate in the response,
 // CreateCert will poll certURL using c.FetchCert, which will result in additional round-trips.
-// In such scenario the caller can cancel the polling with ctx.
+// In such a scenario, the caller can cancel the polling with ctx.
 //
 // CreateCert returns an error if the CA's response or chain was unreasonably large.
 // Callers are encouraged to parse the returned value to ensure the certificate is valid and has the expected features.
@@ -257,7 +257,7 @@ func (c *Client) RevokeCert(ctx context.Context, key crypto.Signer, cert []byte,
 func AcceptTOS(tosURL string) bool { return true }
 
 // Register creates a new account registration by following the "new-reg" flow.
-// It returns registered account. The account is not modified.
+// It returns the registered account. The account is not modified.
 //
 // The registration may require the caller to agree to the CA's Terms of Service (TOS).
 // If so, and the account has not indicated the acceptance of the terms (see Account for details),
