ad65e1b423bb00f2501dfde84040469aef4ee2df
Ian Haken <ihaken@netflix.com>
2018-10-23 09:52:28 -0700

    Removed unneeded sync code in new test.

--- ssh/agent/client_test.go
+++ ssh/agent/client_test.go
@@ -13,7 +13,6 @@ import (
 	"os/exec"
 	"path/filepath"
 	"strconv"
-	"sync"
 	"testing"
 	"time"
 
@@ -233,10 +232,7 @@ func TestServerResponseTooLarge(t *testing.T) {
 	response.Keys = make([]byte, maxAgentResponseBytes+1)
 
 	agent := NewClient(a)
-	var wg sync.WaitGroup
-	wg.Add(1)
 	go func() {
-		defer wg.Done()
 		n, _ := b.Write(ssh.Marshal(response))
 		if n < 4 {
 			t.Fatalf("At least 4 bytes (the response size) should have been successfully written: %d < 4", n)
@@ -249,10 +245,6 @@ func TestServerResponseTooLarge(t *testing.T) {
 	if err.Error() != "agent: client error: response too large" {
 		t.Fatal("Did not get expected error result")
 	}
-	// We need to explicitly close the client connection or else the server-side write call might block waiting for
-	// the client to consume the entire message (which it shouldn't)
-	a.Close()
-	wg.Wait()
 }
 
 func TestAuth(t *testing.T) {
