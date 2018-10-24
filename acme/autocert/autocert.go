5dfe731d732d693f9bdd79c8a74efd30a2f56d0b
David Jones <dxjones@gmail.com>
2018-10-24 11:56:18 -0400

    more informative error "acme/autocert: host %q not configured in HostWhitelist"

--- acme/autocert/autocert.go
+++ acme/autocert/autocert.go
@@ -69,8 +69,7 @@ func HostWhitelist(hosts ...string) HostPolicy {
 	}
 	return func(_ context.Context, host string) error {
 		if !whitelist[host] {
-//			return errors.New("acme/autocert: host not configured")
-			return fmt.Errorf("acme/autocert: host not configured: %q", host)
+			return fmt.Errorf("acme/autocert: host %q not configured in HostWhitelist", host)
 		}
 		return nil
 	}
