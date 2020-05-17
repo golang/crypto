4b4a3535e6e7731b9c7ea3010acb50fa4236892a
Boshi Lian <farmer1992@gmail.com>
2020-05-17 08:31:50 -0700

    add comment

--- ssh/knownhosts/reader.go
+++ ssh/knownhosts/reader.go
@@ -6,6 +6,12 @@ import (
 	"golang.org/x/crypto/ssh"
 )
 
+// NewFromReader creates a host key callback from the given OpenSSH host io.Reader
+// which is in SSH_KNOWN_HOSTS_FILE_FORMAT. The returned callback is for use in
+// ssh.ClientConfig.HostKeyCallback. By preference, the key check
+// operates on the hostname if available, i.e. if a server changes its
+// IP address, the host key check will still succeed, even though a
+// record of the new IP address is not available.
 func NewFromReader(r io.Reader) (ssh.HostKeyCallback, error) {
 	db := newHostKeyDB()
 	if err := db.Read(r, ""); err != nil {
