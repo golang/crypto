84dcc777aaee85807c99afb056f496a3a8b3d8de
Emmanuel T Odeke <emmanuel@orijtech.com>
2020-10-11 21:24:46 -0700

    crypto/ssh: add Client.Close in Dial example
    Fixes golang/go#41906
    
    Change-Id: I2eee82160a4450f531f11d5ba7bd6ed19c019992
    Reviewed-on: https://go-review.googlesource.com/c/crypto/+/261417
    Run-TryBot: Emmanuel Odeke <emm.odeke@gmail.com>
    TryBot-Result: Go Bot <gobot@golang.org>
    Reviewed-by: Roland Shoemaker <roland@golang.org>
    Reviewed-by: Kevin Burke <kev@inburke.com>
    Trust: Roland Shoemaker <roland@golang.org>

--- ssh/example_test.go
+++ ssh/example_test.go
@@ -198,6 +198,7 @@ func ExampleDial() {
 	if err != nil {
 		log.Fatal("Failed to dial: ", err)
 	}
+	defer client.Close()
 
 	// Each ClientConn can support multiple interactive sessions,
 	// represented by a Session.
