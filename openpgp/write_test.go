4eae10a3df86360e3447e664c0743bd091f6ba3d
Geoffrey Casper <gcasper42@gmail.com>
2020-11-01 13:51:04 -0500

    Added testing for the patch

--- openpgp/write_test.go
+++ openpgp/write_test.go
@@ -6,8 +6,11 @@ package openpgp
 
 import (
 	"bytes"
+	"crypto"
+	"golang.org/x/crypto/openpgp/armor"
 	"io"
 	"io/ioutil"
+	"strings"
 	"testing"
 	"time"
 
@@ -269,6 +272,79 @@ func TestEncryption(t *testing.T) {
 				t.Error("signature missing")
 			}
 		}
+	}
+}
+
+func TestSerialEncryption(t *testing.T) {
+	DefaultEncryptionConfig := func() *packet.Config {
+		return &packet.Config{
+			DefaultHash:            crypto.SHA512,
+			DefaultCipher:          packet.CipherAES256,
+			DefaultCompressionAlgo: packet.CompressionNone,
+			RSABits:                4096,
+		}
+	}
+
+	const MessageEncoding = "Message"
+
+	entity, _ := NewEntity("Name", "", "", DefaultEncryptionConfig())
+
+	// Remove Private Keys to share
+	r, w := io.Pipe()
+	defer r.Close()
+	go func() {
+		defer w.Close()
+		entity.Serialize(w)
+	}()
+	publicEntity, err := ReadEntity(packet.NewReader(r))
+	if err != nil {
+		t.Error(err)
+	}
+
+	messageSent := "Some message"
+	content := bytes.NewBuffer([]byte(messageSent))
+
+	println("Creating Message")
+	// Create an Encrypted Message to send
+	to := EntityList{publicEntity}
+	r, w = io.Pipe()
+	defer r.Close()
+
+	go func() {
+		defer w.Close()
+		w2, err := armor.Encode(w, MessageEncoding, make(map[string]string))
+		if err != nil {
+			t.Error(err)
+		}
+		defer w2.Close()
+
+		w3, err := Encrypt(w2, to, nil, nil, DefaultEncryptionConfig())
+		if err != nil {
+			t.Error(err)
+			return
+		}
+		defer w3.Close()
+
+		_, err = io.Copy(w3, content)
+		if err != nil {
+			t.Error(err)
+		}
+	}()
+
+	decode, err := armor.Decode(r)
+	if err != nil {
+		t.Error(err)
+	}
+	messageDetails, err := ReadMessage(decode.Body, EntityList{entity}, nil, DefaultEncryptionConfig())
+	if err != nil {
+		t.Error(err)
+	}
+	messageRead, err := ioutil.ReadAll(messageDetails.UnverifiedBody)
+	if err != nil {
+		t.Error(err)
+	}
+	if strings.Compare(messageSent, string(messageRead)) != 0 {
+		t.Errorf("Message sent isn't the same as message read\n'%s' != '%s'", messageSent, string(messageRead))
 	}
 }
 
