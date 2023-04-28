3ef80564904258d0d9b19a879c805574d184f941
Tobias Klauser <tklauser@distanz.ch>
2023-04-28 11:00:06 +0200

    ssh/test: enable on solaris
    Change-Id: Icf9c867e64ef68f6f46dd7d4cec07cf7c315c2ad
    Reviewed-on: https://go-review.googlesource.com/c/crypto/+/490155
    Reviewed-by: Bryan Mills <bcmills@google.com>
    Reviewed-by: Ian Lance Taylor <iant@google.com>
    Run-TryBot: Tobias Klauser <tobias.klauser@gmail.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Auto-Submit: Tobias Klauser <tobias.klauser@gmail.com>

--- ssh/test/test_unix_test.go
+++ ssh/test/test_unix_test.go
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build aix || darwin || dragonfly || freebsd || linux || netbsd || openbsd || plan9
-// +build aix darwin dragonfly freebsd linux netbsd openbsd plan9
+//go:build aix || darwin || dragonfly || freebsd || linux || netbsd || openbsd || plan9 || solaris
+// +build aix darwin dragonfly freebsd linux netbsd openbsd plan9 solaris
 
 package test
 
