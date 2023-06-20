0d502d7cd64920c6d2cce3950ead89a5c4eb5e69
Roland Shoemaker <roland@golang.org>
2023-06-20 10:54:58 -0700

    x509roots: use "generate" build tag
    Since go generate sets it automatically.
    
    Change-Id: I4623e523392140c0472b250ac99c8c3fa31e5b15
    Reviewed-on: https://go-review.googlesource.com/c/crypto/+/504595
    Auto-Submit: Roland Shoemaker <roland@golang.org>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Reviewed-by: Dmitri Shuralyov <dmitshur@google.com>
    Reviewed-by: Dmitri Shuralyov <dmitshur@golang.org>
    Run-TryBot: Roland Shoemaker <roland@golang.org>

--- x509roots/gen_fallback_bundle.go
+++ x509roots/gen_fallback_bundle.go
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build gen
-// +build gen
+//go:build generate
+// +build generate
 
 //go:generate go run gen_fallback_bundle.go
 
