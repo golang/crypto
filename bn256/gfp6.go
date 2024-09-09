9e92970a1eb41e446822e037016aa89d24c0ce7a
cuishuang <imcusg@gmail.com>
2024-09-09 14:22:39 +0800

    bn256: add missing symbols in comment
    Change-Id: Ibd48a070bd8ce35ef5795a8b73bc4ecac43a993e
    Reviewed-on: https://go-review.googlesource.com/c/crypto/+/611735
    Run-TryBot: shuang cui <imcusg@gmail.com>
    Commit-Queue: Ian Lance Taylor <iant@google.com>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Auto-Submit: Ian Lance Taylor <iant@google.com>
    Reviewed-by: Roland Shoemaker <roland@golang.org>
    TryBot-Result: Gopher Robot <gobot@golang.org>
    Reviewed-by: Ian Lance Taylor <iant@google.com>

--- bn256/gfp6.go
+++ bn256/gfp6.go
@@ -5,7 +5,7 @@
 package bn256
 
 // For details of the algorithms used, see "Multiplication and Squaring on
-// Pairing-Friendly Fields, Devegili et al.
+// Pairing-Friendly Fields", Devegili et al.
 // http://eprint.iacr.org/2006/471.pdf.
 
 import (
