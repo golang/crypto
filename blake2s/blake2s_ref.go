0091fc8e7c693be8d8cbfee206c04512cf4f7513
Michael Pratt <mpratt@google.com>
2025-04-13 22:05:13 -0700

    Revert "blake2s: add loong64 SIMD implementation"
    This reverts CL 661215.
    
    Reason for revert: Does not build on 1.23 or 1.24
    
    Fixes #73354.
    
    Change-Id: I90abd8a3dabf5c0d9fd1062de3ba3e78ecb2f2e8
    Reviewed-on: https://go-review.googlesource.com/c/crypto/+/664796
    Reviewed-by: Carlos Amedee <carlos@golang.org>
    Auto-Submit: Michael Pratt <mpratt@google.com>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>

--- blake2s/blake2s_ref.go
+++ blake2s/blake2s_ref.go
@@ -2,9 +2,15 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build (!amd64 && !386 && !loong64) || !gc || purego
+//go:build (!amd64 && !386) || !gc || purego
 
 package blake2s
+
+var (
+	useSSE4  = false
+	useSSSE3 = false
+	useSSE2  = false
+)
 
 func hashBlocks(h *[8]uint32, c *[2]uint32, flag uint32, blocks []byte) {
 	hashBlocksGeneric(h, c, flag, blocks)
