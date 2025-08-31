559e062ce8bfd6a39925294620b50906ca2a6f95
Nicola Murino <nicola.murino@gmail.com>
2025-08-31 20:07:32 +0200

    ssh/agent: return an error for unexpected message types
    Previously, receiving an unexpected message type in response to a key
    listing or a signing request could cause a panic due to a failed type
    assertion.
    
    This change adds a default case to the type switch in order to detect
    and explicitly handle unknown or invalid message types, returning a
    descriptive error instead of crashing.
    
    Fixes golang/go#75178
    
    Change-Id: Icbc3432adc79fe3c56b1ff23c6724d7a6f710f3a
    Reviewed-on: https://go-review.googlesource.com/c/crypto/+/700295
    Reviewed-by: Roland Shoemaker <roland@golang.org>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Reviewed-by: Michael Pratt <mpratt@google.com>
    Reviewed-by: Jakub Ciolek <jakub@ciolek.dev>

--- ssh/agent/client_test.go
+++ ssh/agent/client_test.go
@@ -7,6 +7,7 @@ package agent
 import (
 	"bytes"
 	"crypto/rand"
+	"encoding/binary"
 	"errors"
 	"io"
 	"net"
@@ -344,6 +345,53 @@ func TestServerResponseTooLarge(t *testing.T) {
 	}
 	if err.Error() != "agent: client error: response too large" {
 		t.Fatal("Did not get expected error result")
+	}
+}
+
+func TestInvalidResponses(t *testing.T) {
+	a, b, err := netPipe()
+	if err != nil {
+		t.Fatalf("netPipe: %v", err)
+	}
+	done := make(chan struct{})
+	defer func() { <-done }()
+
+	defer a.Close()
+	defer b.Close()
+
+	agent := NewClient(a)
+	go func() {
+		defer close(done)
+
+		resp := []byte{agentSuccess}
+		msg := make([]byte, 4+len(resp))
+		binary.BigEndian.PutUint32(msg[:4], uint32(len(resp)))
+		copy(msg[4:], resp)
+
+		if _, err := b.Write(msg); err != nil {
+			t.Errorf("unexpected error sending agent reply: %v", err)
+			b.Close()
+			return
+		}
+
+		if _, err := b.Write(msg); err != nil {
+			t.Errorf("unexpected error sending agent reply: %v", err)
+			b.Close()
+		}
+	}()
+	_, err = agent.List()
+	if err == nil {
+		t.Fatal("error expected")
+	}
+	if !strings.Contains(err.Error(), "failed to list keys, unexpected message type") {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	_, err = agent.Sign(testPublicKeys["rsa"], []byte("message"))
+	if err == nil {
+		t.Fatal("error expected")
+	}
+	if !strings.Contains(err.Error(), "failed to sign challenge, unexpected message type") {
+		t.Fatalf("unexpected error: %v", err)
 	}
 }
 
